09f132f282bee5105764a2eb8cd63954286baeb0
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2025-03-06 08:39:33 +0700

    create banner model

--- app/models/system/banner.model.js
+++ app/models/system/banner.model.js
@@ -8,4 +8,17 @@ const BannerSchema = new mongoose.Schema(
   }
 );
 
+// Middleware to auto-increment bannerCode
+BannerSchema.pre('save', async function(next) {
+  if (!this.bannerCode) {
+    try {
+      const lastBanner = await this.constructor.findOne({}, {}, { sort: { 'bannerCode': -1 } });
+      this.bannerCode = lastBanner ? lastBanner.bannerCode + 1 : 1;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
+});
+
 module.exports = mongoose.model("Banner", BannerSchema);
