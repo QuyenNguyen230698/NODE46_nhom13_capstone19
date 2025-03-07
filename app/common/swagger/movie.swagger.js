62318b005e7c3f267f9c0bfe7a3f5c21e12300f7
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2025-03-07 09:09:40 +0700

    finish swagger user

--- app/common/swagger/movie.swagger.js
+++ app/common/swagger/movie.swagger.js
@@ -2,11 +2,6 @@ const movie = {
     "/api/movies/list": {
         get: {
             tags: ["Movie"],
-            security: [
-                {
-                    bearerAuth: [],
-                },
-            ],
             summary: "Get List Movie",
             description: "Get List Movie",
             responses: {
--- app/common/swagger/user.swagger.js
+++ app/common/swagger/user.swagger.js
@@ -1,12 +1,9 @@
+const { format } = require("morgan")
+
 const user = {
     "/api/users/": {
         get: {
-            tags: ["users"],
-            security: [
-                {
-                    bearerAuth: [],
-                },
-            ],
+            tags: ["Admin or User"],
             summary: "First check run",
             description: "Second check run",
             responses: {
@@ -18,12 +15,7 @@ const user = {
     },
     "/api/users/register": {
         post: {
-            tags: ["users"],
-            security: [
-                {
-                    bearerAuth: [],
-                },
-            ],
+            tags: ["Admin or User"],
             summary: "Register Account",
             description: "Need username, email, password, phoneNumber",
             requestBody: {
@@ -67,7 +59,7 @@ const user = {
     },
     "/api/users/login": {
     post: {
-        tags: ["users"],
+        tags: ["Admin or User"],
         summary: "User login",
         description: "Endpoint for user login",
         requestBody: {
@@ -80,9 +72,12 @@ const user = {
                             email: {
                                 type: "string",
                                 format: "email",
+                                example: "q4@gmail.com"
                             },
                             password: {
                                 type: "string",
+                                format: "password",
+                                example: "1234"
                             }
                         },
                         required: ["email", "password"]
@@ -103,9 +98,111 @@ const user = {
         }
     }
     },
+    "/api/users/login-admin": {
+    post: {
+        tags: ["Admin or User"],
+        summary: "Admin login",
+        description: "Endpoint for Admin login",
+        requestBody: {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: {
+                        type: "object",
+                        properties: {
+                            email: {
+                                type: "string",
+                                format: "email",
+                                example: "q@gmail.com"
+                            },
+                            password: {
+                                type: "string",
+                                format: "password",
+                                example: "1234"
+                            }
+                        },
+                        required: ["email", "password"]
+                    }
+                }
+            }
+        },
+        responses: {
+            "200": {
+                description: "Login successful",
+            },
+            "400": {
+                description: "Bad Request",
+            },
+            "401": {
+                description: "Unauthorized",
+            }
+        }
+    }
+    },
+    "/api/users/update": {
+        post: {
+            tags: ["Admin or User"],
+            summary: "Update for users",
+            description: "Update users. Requires Bearer Token authentication.",
+            security: [
+                {
+                    bearerAuth: []
+                }
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "object",
+                            properties: {
+                                email: {
+                                    type: "string",
+                                    format: "email",
+                                },
+                                password: {
+                                    type: "string",
+                                    format: "password",
+                                    example: "1234"
+                                },
+                                username: {
+                                    type: "string",
+                                    example: "full name"
+                                },
+                                phoneNumber: {
+                                    type: "string",
+                                    example: "0909988776"
+                                },
+                                avatar: {
+                                    type: "string",
+                                    example: "https://res.cloudinary.com/dy3tzo2kg/image/upload/v1741069754/avatar-default_q3pzgr.png"
+                                },
+                                position: {
+                                    type: "string",
+                                    example: "Guest or Staff"
+                                }
+                            },
+                            required: ["email", "password", "username", "phoneNumber", "avatar", "position"]
+                        }
+                    }
+                }
+            },
+            responses: {
+                "200": {
+                    description: "Change Roles Successful",
+                },
+                "400": {
+                    description: "Bad Request",
+                },
+                "401": {
+                    description: "Unauthorized",
+                }
+            }
+        }
+    },
     "/api/users/list": {
     get: {
-        tags: ["users"],
+        tags: ["Admin or User"],
         summary: "Get list of users",
         description: "Retrieve a list of users. Requires Bearer Token authentication.",
         security: [
@@ -128,6 +225,134 @@ const user = {
         }
     }
     },
+    "/api/users/update-role": {
+        post: {
+            tags: ["Admin or User"],
+            summary: "Get list of users",
+            description: "Retrieve a list of users. Requires Bearer Token authentication.",
+            security: [
+                {
+                    bearerAuth: []
+                }
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "object",
+                            properties: {
+                                email: {
+                                    type: "string",
+                                    format: "email",
+                                },
+                                roles: {
+                                    type: "string",
+                                    example: "ADMIN or USER"
+                                }
+                            },
+                            required: ["email", "role"]
+                        }
+                    }
+                }
+            },
+            responses: {
+                "200": {
+                    description: "Change Roles Successful",
+                },
+                "400": {
+                    description: "Bad Request",
+                },
+                "401": {
+                    description: "Unauthorized",
+                }
+            }
+        }
+    },
+    "/api/users/delete": {
+        post: {
+            tags: ["Admin or User"],
+            summary: "Delete users",
+            description: "Delete users. Requires Bearer Token authentication.",
+            security: [
+                {
+                    bearerAuth: []
+                }
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "object",
+                            properties: {
+                                email: {
+                                    type: "string",
+                                    format: "email",
+                                },
+                                status: {
+                                    type: "string",
+                                    example: "ACTIVE or INACTIVE"
+                                }
+                            },
+                            required: ["email", "status"]
+                        }
+                    }
+                }
+            },
+            responses: {
+                "200": {
+                    description: "Change Roles Successful",
+                },
+                "400": {
+                    description: "Bad Request",
+                },
+                "401": {
+                    description: "Unauthorized",
+                }
+            }
+        }
+    },
+    "/api/users/find-user": {
+        post: {
+            tags: ["Admin or User"],
+            summary: "Find users",
+            description: "Find users. Requires Bearer Token authentication.",
+            security: [
+                {
+                    bearerAuth: []
+                }
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    "application/json": {
+                        schema: {
+                            type: "object",
+                            properties: {
+                                email: {
+                                    type: "string",
+                                    format: "email",
+                                }
+                            },
+                            required: ["email"]
+                        }
+                    }
+                }
+            },
+            responses: {
+                "200": {
+                    description: "Change Roles Successful",
+                },
+                "400": {
+                    description: "Bad Request",
+                },
+                "401": {
+                    description: "Unauthorized",
+                }
+            }
+        }
+    }
 }
 
 module.exports = user
